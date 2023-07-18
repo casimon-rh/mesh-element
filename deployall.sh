351c6805a776d5f08c6220b4032b7c044d569a3c
Carlos Simon <casimon@redhat.com>
2023-07-18 15:49:57 -0600

    deploy no-sm

--- deployall.sh
+++ deployall.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
 helm upgrade --install aa . -f values/aa.yaml
-helm upgrade --install bb . -f values/bb.yaml
-helm upgrade --install cc . -f values/cc.yaml
-helm upgrade --install dd . -f values/dd.yaml
-helm upgrade --install ee . -f values/ee.yaml
-helm upgrade --install ff . -f values/ff.yaml
-helm upgrade --install gg . -f values/gg.yaml
-helm upgrade --install hh . -f values/hh.yaml
